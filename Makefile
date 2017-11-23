3155c0b7230dc736eb487b7ec4ecab98d858e793
Erik Scharwächter <erik.scharwaechter@hpi.de>
2017-11-23 17:03:58 +0100

    cleaner Makefile and setup.py

--- Makefile
+++ Makefile
@@ -1,16 +1,15 @@
-all: python2
-
-python2:
-	python setup.py build
-	sudo python setup.py install
-
-python3:
+all:
+	python2 setup.py build
 	python3 setup.py build
+
+install:
+	sudo python2 setup.py install
 	sudo python3 setup.py install
+
+clean:
+	python2 setup.py clean
+	python3 setup.py clean
 
 gfz:
 	python setup.py build
 	python setup.py install --user
-
-clean:
-	rm -rf build/
--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
 from distutils.core import setup, Extension
-import numpy
+import numpy, os
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 mod = Extension('BlockCorr',
-    include_dirs = [numpy.get_include()],
-    sources = ['BlockCorr.c', 'list.c', 'PyBlockCorr.c'],
-    extra_compile_args=['-fopenmp','-O3','-march=native','-mavx','-funroll-loops'],
-    #extra_compile_args=['-fopenmp','-O3'],
-    #extra_compile_args=['-fopenmp','-g','-pg'],
-    extra_link_args=['-lgomp']
+  include_dirs = [numpy.get_include()],
+  sources = ['BlockCorr.c', 'list.c', 'PyBlockCorr.c'],
+  extra_compile_args=['-fopenmp','-O3','-march=native','-mavx','-funroll-loops'],
+  #extra_compile_args=['-fopenmp','-O3'],
+  #extra_compile_args=['-fopenmp','-g','-pg'],
+  extra_link_args=['-lgomp']
 )
 
-setup (name = 'BlockCorr',
-    author = 'Erik Scharwaechter',
-    author_email = 'erik.scharwaechter@hpi.de',
-    ext_modules = [mod]
+setup(
+  name = 'BlockCorr',
+  version = '0.3',
+  author = 'Erik Scharwaechter',
+  author_email = 'erik.scharwaechter@hpi.de',
+  license = 'MIT',
+  long_description=read('README'),
+  ext_modules = [mod]
 )
